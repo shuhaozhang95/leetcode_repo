a1552953ac8b04a0277461080e5f8962669d07fa
Yucheng Ji <yuchengji@Yuchengs-MacBook-Pro.local>
2020-05-26 21:53:14 +0800

    update 234

--- linkedList/linkedList_py/code/234_palindromeLinkedList.py
+++ linkedList/linkedList_py/code/234_palindromeLinkedList.py
@@ -14,6 +14,51 @@ class Solution:
 
     ### Method 2: Recursive function method
     def isPalindrome02(self, head: ListNode) -> bool:
+        self.front = head 
+        def recursive_check(node):
+            if node:
+                if not recursive_check(node.next):
+                    return False
+                if self.front.val != node.val:
+                    return False
+                self.front = self.front.next
+            return True
+        return recursive_check(head)
 
-    ### Method 3: 
+    ### Method 3: reverse the second half and check
     def isPalindrome03(self, head: ListNode) -> bool:
+        if not head: return True
+        if not head.next: return True
+
+        firstEnd = self.firstPartEnd(head)
+        secondHead = self.reverseLL(firstEnd.next)
+
+        res = True
+        firstPosition = head
+        secondPosition = secondHead
+        while res and secondPosition:
+            if firstPosition.val != secondPosition.val:
+                res = False
+            firstPosition = firstPosition.next
+            secondPosition = secondPosition.next
+        firstEnd.next = self.reverseLL(secondHead)
+        return res
+
+    def reverseLL(self, head):
+        prev = None
+        current = head
+        while current:
+            nex = current.next
+            current.next = prev
+            prev = current
+            current = nex
+        return prev
+
+    def firstPartEnd(self, head):
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
